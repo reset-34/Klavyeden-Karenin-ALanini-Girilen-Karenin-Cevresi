a05d66a523e739928c98a6e1613885e53615b374
Serap BALCI <81075168+reset-34@users.noreply.github.com>
2024-08-01 15:48:42 +0300

    Add project files.

---
+++ Klavyeden Karenin ALanini Girilen Karenin Cevresi/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klavyeden_Karenin_ALanini_Girilen_Karenin_Cevresi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int kenar, cevre;
+
+            Console.WriteLine("Karenin bir kenarını giriniz");
+            kenar = Int32.Parse(Console.ReadLine());
+
+            cevre = kenar * 4;
+
+            Console.WriteLine("Karenin Cevresi = " + cevre);
+            Console.ReadLine();
+
+        }
+    }
+}
